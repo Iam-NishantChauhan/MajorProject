75d9b03326fd366ae60f103f646a26b301928f75
Nishant Chauhan <nishantpssn720@gmail.com>
2023-10-02 10:29:43 +0530

    first commit

--- src/Component/AfterSignIn/SearchBar.js
+++ src/Component/AfterSignIn/SearchBar.js
@@ -6,14 +6,14 @@ import { BsSun } from "react-icons/bs";
 import { motion } from "framer-motion";
 
 const SearchBar = () => {
-  let [isOpen, setIsOpen] = useState(false);
-  // function closeModal() {
-  //   setIsOpen(false);
-  // }
+  // let [isOpen, setIsOpen] = useState(false);
+  // // function closeModal() {
+  // //   setIsOpen(false);
+  // // }
 
-  function openModal() {
-    setIsOpen(true);
-  }
+  // function openModal() {
+  //   setIsOpen(true);
+  // }
   return (
     <>
       <div className="bg-cSearchBar flex justify-between w-full h-20 shadow-[0px_5px_20px_rgba(0,0,0,0.15)] ">
@@ -33,7 +33,7 @@ const SearchBar = () => {
           <div className=" pl-5 text-lg font-semibold">
             <button
               type="button"
-              onClick={openModal}
+              // onClick={openModal}
               className="px-2 py-2 rounded-3xl font-bold  text-white hover:text-white hover:bg-[#f8777d] "
             >
               Create
